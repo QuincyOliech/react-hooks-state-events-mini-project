de6b1c98b6bb1f0bf4c7b82128f77567e1f822cd
Linet Oliech <linetaoliech@gmail.com>
2023-01-13 23:54:48 +0300

    assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+   function toggleSelection(e){
+    const buttons =Array.from(document.querySelectorAll(".buttons"))
+    buttons.forEach(button=>{
+      button.classList.remove("selected");
+   });
+   e.target.classList.add("selected");
+   }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category,index)=>{
+        return <button className="buttons" onClick={toggleSelection} key={index}>{category}</button>
+      })
+      }
     </div>
   );
 }
